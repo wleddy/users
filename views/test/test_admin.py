5d28fa45dede64ce12111e094ee44bbaf120ca85
Bill Leddy <bill@williesworkshop.net>
2018-08-24 14:16:45 -0700

    Fix typo

--- views/test/test_admin.py
+++ views/test/test_admin.py
@@ -23,10 +23,10 @@ def delete_test_db():
                 
 def test_admin():
    admin = Admin(db)
-   admin.register(User,'/user/',display_name='Users',minimum_rank_required=5000,role_list=['admin',])
+   admin.register(User,'/user/',display_name='Users',minimum_rank_required=500,role_list=['admin',])
    admin.register(Role,'/role/',display_name='User Permissions',minimum_rank_required=2000)
    assert len(admin.permissions) == 2
-      
+   
    assert admin.has_access('admin') == True
    assert admin.has_access('admin',User) == True
    assert admin.has_access('admin',Role) == False
