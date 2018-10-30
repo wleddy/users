22f0bc6fa2ffe5bccf41a2b451382bfc9a7badee
Bill Leddy <bill@williesworkshop.net>
2018-10-30 14:22:14 -0700

    add iso_date_string filter

--- jinja_filters.py
+++ jinja_filters.py
@@ -1,21 +1,15 @@
-from date_utils import getDatetimeFromString
+from date_utils import getDatetimeFromString, date_to_string
 from datetime import datetime
 
 # some custom filters for templates
-
-
-def short_date_string(value, format='%m/%d/%y'):
-    if value is None or value == "":
-        # test if value is None or the empty string
-        return value
-
-    if type(value) is datetime:
-        return value.strftime(format)
-    if type(value) is str:
-        return getDatetimeFromString(value).strftime(format)
-    
-    return value
-    
+def iso_date_string(value):
+    format = '%Y-%m-%d'
+    return date_to_string(value,format)
+        
+        
+def short_date_string(value):
+    format='%m/%d/%y'
+    return date_to_string(value,format)
     
 def two_digit_string(the_string):
     #import pdb;pdb.set_trace()
@@ -35,3 +29,4 @@ def register_jinja_filters(app):
     # register the filters
     app.jinja_env.filters['short_date_string'] = short_date_string
     app.jinja_env.filters['money'] = two_digit_string
+    app.jinja_env.filters['iso_date_string'] = iso_date_string
